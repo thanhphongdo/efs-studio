38f29d8a80e3d88096cec62d1f0f545d56f2fe62
phong-do-tpv-mti <phong.do.tpv@one-line.com>
2024-09-09 14:41:02 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: "export",
     // distDir: '../../efs-studio',
-    basePath: '/efs-studio',
-    assetPrefix: '/efs-studio/',
+    // basePath: '/efs-studio',
+    // assetPrefix: '/efs-studio/',
 }
 
 module.exports = nextConfig
