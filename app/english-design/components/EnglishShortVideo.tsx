432a82b150ac005543e556cf79a59b195482d465
phong-do-tpv-mti <phong.do.tpv@one-line.com>
2024-09-18 15:09:02 +0700

    update UI splited content

--- app/english-design/components/EnglishShortVideo.tsx
+++ app/english-design/components/EnglishShortVideo.tsx
@@ -56,10 +56,11 @@ export const EnglishShortVideo = (
       )}
       {currentSlide()!.uuid === "MAIN" && !!currentSlide()!.splitedContent && (
         <span className="tw-absolute tw-right-4 tw-bottom-4 tw-z-[100000000] tw-text-4xl tw-bg-white tw-text-blue-500 tw-rounded-md tw-px-4 tw-font-bold">
-          {currentSlide()!.part ??
-            0 * currentSlide()!.splitedContent! +
-              (currentSlide()!.contentIndex + 1)}
-          /{currentSlide()!.contentTotal ?? 0}
+          {((currentSlide()!.part ?? 1) - 1) * currentSlide()!.splitedContent! +
+            (currentSlide()!.contentIndex +
+              1 +
+              (currentSlide()!.startIndex ?? 0))}
+          /{currentSlide()!.contentTotal ?? currentSlide()!.contents.length}
         </span>
       )}
     </div>
